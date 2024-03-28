5ae1b314da8ff86bd20828bda1b2c1d983ff8195
Alexandros Tzimas <alex.tzimas@mystenlabs.com>
2024-03-28 16:08:58 +0200

    Update footer

--- app/page.tsx
+++ app/page.tsx
@@ -47,7 +47,7 @@ export default async function Home() {
               className="mt-6 animate-fade-up text-center text-gray-500 opacity-0 [text-wrap:balance] md:text-xl"
           style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
         >
-          A collection of mini games, to inspire the community of the Sui ecosystem.
+          A collection of mini games, to inspire the community of Sui.
         </p>
         <div
             className="mx-auto mt-6 flex animate-fade-up items-center justify-center space-x-5 opacity-0"
