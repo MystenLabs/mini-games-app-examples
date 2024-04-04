6429423bcbb032a682fc5602c406ec9f33d44a87
Alexandros Tzimas <alex.tzimas@mystenlabs.com>
2024-04-04 14:50:31 +0300

    Add disclaimer on navbar

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -12,24 +12,17 @@ export default function NavBar({ session }: { session: Session | null }) {
 
   return (
     <>
-      <div
-        className={`fixed top-0 w-full flex justify-center ${
-          scrolled
-            ? "border-b border-gray-200 bg-white/50 backdrop-blur-xl"
-            : "bg-white/0"
-        } z-30 transition-all`}
-      >
-        <div className="mx-5 flex h-16 max-w-screen-xl items-center justify-between w-full">
-          <Link href="https://sui.io/" className="flex items-center font-display text-2xl">
-            <Image
-              src={suilogo}
-              alt="Sui logo"
-              height="30"
-              className="mr-2 rounded-sm"
-            ></Image>
-          </Link>
+        <div
+            className={`fixed top-0 w-full flex justify-evenly ${
+                scrolled
+                    ? "border-b border-gray-200 bg-transparent/50 backdrop-blur-xl"
+                    : "bg-white/0"
+            } z-30 transition-all`}
+        >
+            <div className="mx-5 flex h-16 items-center justify-end text-white ">
+                <span>The mini games are published on testnet, and not intended for real money gambling.</span>
+            </div>
         </div>
-      </div>
     </>
   );
 }
