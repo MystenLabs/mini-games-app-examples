11831e6868120452138f290cdc054bc452b0ba50
Alexandros Tzimas <alex.tzimas@mystenlabs.com>
2024-04-04 13:11:32 +0300

    Update twitter button

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -10,11 +10,12 @@ export default function Footer() {
               href="https://twitter.com/SuiNetwork"
               target="_blank"
               rel="noreferrer"
-              className="mx-auto mb-5 flex max-w-fit animate-fade-up items-center justify-center space-x-2 overflow-hidden rounded-full bg-black px-7 py-2 transition-colors hover:opacity-70"
+              className="mx-auto mb-5 flex max-w-fit animate-fade-up items-center justify-center space-x-2 overflow-hidden rounded-full px-7 py-2 transition-colors hover:opacity-70"
+              style={{backgroundColor: "rgb(75,163,251)"}}
           >
               <Twitter className="text-[#1d9bf0]"/>
               <p className="text-sm font-semibold text-white">
-                  Follow us on X.com
+                  Follow us on Twitter
               </p>
           </a>
       </div>
