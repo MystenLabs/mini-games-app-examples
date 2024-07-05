7d6f5a119a8bb101cb8ed1059352334d62f9a505
Sang Kim <nonvis@gmail.com>
2024-07-05 11:22:54 +0900

    Revert "Update route.ts"
    This reverts commit 6f69e93b27071de797175cc0c57724f55809430c.

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-const authOptions: NextAuthOptions = {
+export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
